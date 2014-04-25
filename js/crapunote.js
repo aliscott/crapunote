38b3705e2e6d433289e118ac84426a3cf1c472ff
Alistair Scott <alistair@rightscale.com>
2014-04-25 18:02:01 +0100

    Maintain state when a task is deleted.

--- js/crapunote.js
+++ js/crapunote.js
@@ -1,13 +1,14 @@
 (function($) {
   var storyRowSelector = 'tr.level_1';
   var taskRowSelector = 'tr.level_2';
+  var expandedStories = [];
 
   function addExpandAnchor(tr) {
     var descriptionDiv = $(tr).find('.description');
     descriptionDiv.parent().prepend(
       $('<a></a>')
         .attr('href', '#')
-        .html('<div class="slider_icon sprite-right_arrow"></div>')
+        .html('<div class="slider_icon sprite-down_arrow"></div>')
         .click(function(e) {
           toggleRow(tr);
           e.preventDefault();
@@ -18,6 +19,7 @@
   function addExpandAllAnchors() {
     $('#task_table thead .title').append(
       $('<a></a>')
+        .addClass('expand_all')
         .attr('href', '#')
         .text('expand all')
         .click(function(e) {
@@ -28,6 +30,7 @@
     .append('<span> / </span>')
     .append(
       $('<a></a>')
+        .addClass('collapse_all')
         .attr('href', '#')
         .text('collapse all')
         .click(function(e) {
@@ -46,19 +49,24 @@
   }
 
   function isRowExpanded(tr) {
-    return $(tr).hasClass('expanded')
+    return !$(tr).hasClass('collapsed')
   }
 
   function expandRow(tr) {
     $(tr).nextUntil(storyRowSelector, taskRowSelector).show();
     $(tr).find('.slider_icon').removeClass('sprite-right_arrow').addClass('sprite-down_arrow');
-    $(tr).addClass('expanded');
+    $(tr).removeClass('collapsed');
+    expandedStories.push(tr.id);
   }
 
   function collapseRow(tr) {
     $(tr).nextUntil(storyRowSelector, taskRowSelector).hide();
     $(tr).find('.slider_icon').removeClass('sprite-down_arrow').addClass('sprite-right_arrow');
-    $(tr).removeClass('expanded');
+    $(tr).addClass('collapsed');
+    var rowIndex = $.inArray(tr.id, expandedStories);
+    if (rowIndex !== -1) {
+      expandedStories.splice(rowIndex, 1);
+    }
   }
 
   function toggleRow(tr) {
@@ -70,16 +78,27 @@
   }
 
   function setupExpandAnchors() {
-    collapseAll();
     addExpandAllAnchors();
     $(storyRowSelector).each(function(i, tr) {
       if ($(tr).nextUntil(storyRowSelector, taskRowSelector).length > 0) {
         addExpandAnchor(tr);
       }
+      if ($.inArray(tr.id, expandedStories) === -1) {
+        collapseRow(tr);
+      }
     });
+  }
+
+  function isExpandAnchorsSetup() {
+    return $('.expand_all').length >= 1;
   }
 
   $(document).ready(function() {
     setupExpandAnchors();
+    $('#main').on('DOMSubtreeModified', '#task_table', function() {
+      if (!isExpandAnchorsSetup()) {
+        setupExpandAnchors();
+      }
+    });
   });
 })(jQuery);
